115b8bf1e1c85bcf75bcc118fec68c3da807468c
Avery Zakson <avery.zakson@gmail.com>
2021-01-28 10:30:30 -0600

    Initial Commit

---
+++ To Do List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  To Do List
+//
+//  Created by Avery Zakson on 1/28/21.
+//  Copyright © 2021 Avery Zakson. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
